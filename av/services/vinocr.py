b9a1aa4c41a16bfe5fe98a86dcbaa51c4461b289
Breno Silva <brenokcc@yahoo.com.br>
2023-06-11 10:51:01 -0300

    Continuação da implementação

--- av/services/vinocr.py
+++ av/services/vinocr.py
@@ -8,12 +8,12 @@ print(Service().detect_vin(url))
 """
 
 class Service():
-    def detect_vin(self, uri):
-        data = {"url": uri, "token": os.environ['VINOCR_TOKEN']}
+    def detect_vin(self, url):
+        data = {"url": url, "token": os.environ['VINOCR_TOKEN']}
         data = requests.post('http://vinocr.aplicativo.click', json=data).json()
         print(data)
         return data.get('vin')
 
     def test(self):
         url = 'https://www.recognition.ws/img/samples/vinocr26.jpg'
-        return self.detect_vin() == '3FADP4GX7GM105302'
+        return self.detect_vin(url) == '3FADP4GX7GM105302'
